88f02a7c0f8053bd0a9acb9925f09bd57af32ed2
AdkhamjonKhamidov <136289251+AdkhamjonKhamidov@users.noreply.github.com>
2025-08-04 16:56:12 -0400

    shooter-spawner

--- Scripts/spawn_warning.gd
+++ Scripts/spawn_warning.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var enemy_type: PackedScene
+@onready var enemy_shooter = preload("res://Scenes/Game/Enemy/shooter.tscn")
 @onready var sprite = $"AnimatedSprite2D"
 
 func _ready() -> void:
@@ -10,4 +11,8 @@ func _on_timeout() -> void:
 	var enemy = enemy_type.instantiate()
 	enemy.position = position
 	$"/root/Base".add_child(enemy)
+	queue_free()
+	var shooter = enemy_shooter.instantiate()
+	shooter.position = position
+	$"/root/Base".add_child(shooter)
 	queue_free()
